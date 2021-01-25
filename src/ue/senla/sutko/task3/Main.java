cd51302bd9eb811a9df96f31d6b1f38587227dda
SUTIK <SUTIK90@MAIL.RU>
2021-01-25 10:53:24 +0300

    4 задачи

--- src/ue/senla/sutko/task3/Main.java
+++ src/ue/senla/sutko/task3/Main.java
@@ -2,6 +2,7 @@ package ue.senla.sutko.task3;
 
 public class Main {
     public static void main(String[] args) {
+        //первая задача
         int count=0;
         while (count<10){
 
@@ -10,6 +11,7 @@ public class Main {
             count++;
         }
 
+        //вторая задача
         for (int i=0;i<10;i++){
 
             Cat Cat2= new Cat("Murca",4);
@@ -17,6 +19,8 @@ public class Main {
             Cat2.setCatAge(2);
             System.out.println("Имя кота-"+Cat2.getCatName() +" Возраст-"+Cat2.getCatAge());
         }
+
+        //третья задача
         count=0;
         do {
             Cat Cat3 =new Cat("Boris",5);
@@ -24,5 +28,11 @@ public class Main {
             count++;
         }while (count<10);
 
+        //четвертая задача
+        Cat array[] = new Cat[5] ;
+        for (Cat d:array) {
+        Cat Cat4 = new Cat("Mur",12);
+        System.out.println("Имя кота-"+Cat4.getCatName() +" Возраст-"+Cat4.getCatAge());
+        }
     }
 }
